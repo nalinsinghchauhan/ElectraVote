b4928ab9c482cdfd9fdeb37b213f62ca2181c558
Nalin Singh Chauhan <151433974+nalinsinghchauhan@users.noreply.github.com>
2025-05-04 23:43:08 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
-}
-
 export default defineConfig({
   schema: "./shared/schema.ts",
   out: "./migrations",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: process.env.DATABASE_URL + "?sslmode=require"
-  }
+  driver: "pg"
 });
